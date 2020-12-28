9e7658582ccbcfb86e83138bf9946818fc883a47
Carrick <skeyic@foxmail.com>
2020-12-28 20:56:09 +0800

    Add mergeObjects

---
+++ app/utils/mergeObjects.go
@@ -0,0 +1,49 @@
+package utils
+
+type ToMergeObject interface {
+	ID() int
+}
+
+// Descend arrays
+func MergeDescendObjects(sourceObjects, newObjects []ToMergeObject) (lastObjects []ToMergeObject) {
+	var (
+		newAnchor, sourceAnchor = 0, 0
+		newLength, sourceLength = len(newObjects), len(sourceObjects)
+	)
+
+	if newLength == 0 {
+		return sourceObjects
+	}
+
+	if sourceLength == 0 {
+		return newObjects
+	}
+
+	for {
+		if newAnchor < newLength && sourceAnchor < sourceLength {
+			if sourceObjects[sourceAnchor].ID() < newObjects[newAnchor].ID() {
+				lastObjects = append(lastObjects, newObjects[newAnchor])
+				newAnchor++
+			} else if sourceObjects[sourceAnchor].ID() == newObjects[newAnchor].ID() {
+				lastObjects = append(lastObjects, newObjects[newAnchor])
+				newAnchor++
+				sourceAnchor++
+			} else {
+				lastObjects = append(lastObjects, sourceObjects[sourceAnchor])
+				sourceAnchor++
+			}
+		}
+
+		if newAnchor == newLength {
+			lastObjects = append(lastObjects, sourceObjects[sourceAnchor:]...)
+			break
+		}
+
+		if sourceAnchor == sourceLength {
+			lastObjects = append(lastObjects, newObjects[newAnchor:]...)
+			break
+		}
+	}
+
+	return
+}
---
+++ app/utils/mergeObjects_test.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Student struct {
+	id int
+}
+
+func (s Student) ID() int {
+	return s.id
+}
+
+func TestMergeDescendObjects(t *testing.T) {
+	var (
+		s1 = []ToMergeObject{
+			Student{
+				id: 20,
+			},
+			Student{
+				id: 19,
+			},
+			Student{
+				id: 15,
+			},
+			Student{
+				id: 13,
+			},
+		}
+		s2 = []ToMergeObject{
+			Student{
+				id: 13,
+			},
+			Student{
+				id: 4,
+			},
+			Student{
+				id: 3,
+			},
+			Student{
+				id: 2,
+			},
+		}
+	)
+
+	sl := MergeDescendObjects(s1, s2)
+	for _, s := range sl {
+		fmt.Print(s.ID(), ",")
+	}
+	fmt.Println()
+}
